502392f43c3667b839bd91ecffb234100dffa8bb
Preston <preston.huang28@gmail.com>
2026-08-11 15:47:30 -0400

    worked on the filter buttons and messed with the colors of the website

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Responsive navigation bar
 const nav = document.querySelector('nav');
 const menu = document.getElementById('menu');
 
@@ -9,5 +10,15 @@ function toggleMenu() {
 document.querySelectorAll('#menu a').forEach(link => {
     link.addEventListener('click', () => {
         if (menu.classList.contains('show')) toggleMenu();
+    });
+});
+
+//filter buttons active toggle
+const buttons = document.querySelectorAll(".filters button");
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        buttons.forEach(btn => btn.classList.remove("active"));
+        button.classList.add("active");
     });
 });
