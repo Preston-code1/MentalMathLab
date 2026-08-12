8d8b8178d40751a937e950f334450d45c3068743
Preston <preston.huang28@gmail.com>
2026-08-12 09:21:10 -0400

    made the filter buttons work

--- script.js
+++ script.js
@@ -15,10 +15,23 @@ document.querySelectorAll('#menu a').forEach(link => {
 
 //filter buttons active toggle
 const buttons = document.querySelectorAll(".filters button");
+const cards = document.querySelectorAll(".card");
 
 buttons.forEach(button => {
     button.addEventListener("click", () => {
+        const filter = button.dataset.filter;
+
         buttons.forEach(btn => btn.classList.remove("active"));
         button.classList.add("active");
+
+        cards.forEach(card => {
+            const category = card.dataset.category;
+
+            if (filter === "all" || category === filter) {
+                card.style.display = "block";
+            } else {
+                card.style.display = "none";
+            }
+        })
     });
 });
